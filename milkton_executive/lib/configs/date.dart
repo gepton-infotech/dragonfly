0a79bc5b13a4e87b130012fd2615f07701c29d66
abhibhaw <asthana3110@gmail.com>
2021-06-15 22:58:07 +0530

    Changed display date to tomorrow after 8:30
    - Fixes #2
    - Increase ver for playstore release

--- milkton_executive/lib/configs/date.dart
+++ milkton_executive/lib/configs/date.dart
@@ -1,3 +1,4 @@
 final DateTime _today = new DateTime.now();
+final _serverDate = _today.add(Duration(minutes: 210));
 final String today =
-    "${_today.day.toString().padLeft(2, '0')}-${_today.month.toString().padLeft(2, '0')}-${_today.year.toString().padLeft(2, '0')}";
+    "${_serverDate.day.toString().padLeft(2, '0')}-${_serverDate.month.toString().padLeft(2, '0')}-${_serverDate.year.toString().padLeft(2, '0')}";
