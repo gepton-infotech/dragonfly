66af4bacf12f00bf08a5459f50eb0f6ae1d54dbb
abhibhaw <39991296+abhibhaw@users.noreply.github.com>
2023-12-17 14:37:47 +0530

    feat: Adds flavor for android project

--- milkton_executive/lib/presentation/screens/splash.dart
+++ milkton_executive/lib/presentation/screens/splash.dart
@@ -1,11 +1,9 @@
 import 'package:flutter/material.dart';
-import 'package:milkton_executive/presentation/screens/login.dart';
 
 class SplashScreen extends StatelessWidget {
   const SplashScreen({super.key});
-
   @override
   Widget build(BuildContext context) {
-    return const LoginScreen();
+    return const Scaffold(body: Column());
   }
 }
